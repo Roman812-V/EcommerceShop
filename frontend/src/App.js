09afd664c6054cd6be833995b1de9c6b7acf49f7
Roman Volchuk <volchukr@gmail.com>
2021-08-10 17:55:04 +0200

    started, addded product component

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import Product from './components/Product';
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="/">Sweet Spot</a>
+        </div>
+        <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+        </div>
+    </header>
+    <main>
+        <div className="row center">
+            {data.products.map(product=>(
+                <Product key={product._id} product={product}></Product>    
+                ))}           
+        </div>
+    </main>
+   <footer className="row center">All rights reserved by Roman Volchuk.</footer>
+</div>
+  );
+}
+
+export default App;
