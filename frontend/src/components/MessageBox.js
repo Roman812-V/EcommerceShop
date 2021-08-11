014e39295006440fe06811fe7b16264bf4b29047
Roman Volchuk <volchukr@gmail.com>
2021-08-11 23:54:52 +0200

    added redux to home screen and product screen

--- frontend/src/components/MessageBox.js
+++ frontend/src/components/MessageBox.js
@@ -1,9 +1,10 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from 'react';
 
 export default function MessageBox(props) {
-    return (
-        <div className={`alert alert-${props.variant || 'info'}`}>
-            {props.children}
-        </div>
-    )
+  return (
+    <div className={`alert alert-${props.variant || 'info'}`}>
+      {props.children}
+    </div>
+  );
 }
