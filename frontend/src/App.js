ece0698806d69fd00ce7ecaf2b21a3cfb90f6433
Roman Volchuk <volchukr@gmail.com>
2021-08-12 16:48:06 +0200

    added adding to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route} from 'react-router-dom'
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-      <BrowserRouter>
-    <div className="grid-container">
-    <header className="row">
-        <div>
-            <a className="brand" href="/">Sweet Spot</a>
-        </div>
-        <div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Sweet Spot
+            </a>
+          </div>
+          <div>
             <a href="/cart">Cart</a>
             <a href="/signin">Sign In</a>
-        </div>
-    </header>
-    <main>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/" component={HomeScreen} exact></Route>
-        
-    </main>
-   <footer className="row center">All rights reserved by Roman Volchuk.</footer>
-</div>
-</BrowserRouter>
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">
+          All rights reserved by Roman Volchuk.
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
